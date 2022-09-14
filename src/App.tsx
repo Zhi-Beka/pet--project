45ee986d540b3f71333d5fb33c266fc151c18c3a
jibek <jibek260886@gmail.com>
2022-09-14 19:58:02 +0600

    feat: add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
 import Counter from "./components/Counter";
 import "./index.scss";
+import { AboutPageLazy } from "./pages/aboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/mainPage/MainPage.lazy";
 
 const App = () => {
   return (
     <div className="App">
-      gfjkbhsfgl;ijlhh
-      <Counter />
+      <Link to={"/"}>MainPage</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageLazy />} />
+          <Route path="/about" element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };

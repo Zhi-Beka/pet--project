f8d4ba79a6022f7d2ca3c0c4c11f118f14153762
jibek <jibek260886@gmail.com>
2022-09-14 21:05:44 +0600

    feat: add simple theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-import { Suspense } from "react";
-import Counter from "./components/Counter";
-import "./index.scss";
+import { Suspense, useState } from "react";
 import { AboutPageLazy } from "./pages/aboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/mainPage/MainPage.lazy";
 
+export enum Theme {
+  DARK = "dark",
+  LIGHT = "light",
+}
+
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  };
   return (
-    <div className="App">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}> change color</button>
       <Link to={"/"}>MainPage</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>

ddd07149e4f75be8a010a6cb2936fc7277b68cf0
jibek <jibek260886@gmail.com>
2022-09-15 16:17:21 +0600

    feat: add classNames function

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { Suspense } from "react";
 import { AboutPageLazy } from "./pages/aboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/mainPage/MainPage.lazy";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}> change color</button>
       <Link to={"/"}>MainPage</Link>
       <Link to={"/about"}>About</Link>

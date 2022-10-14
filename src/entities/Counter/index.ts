0d626119b4c78afa63f54a3b3050064883da0319
jibek <jibek260886@gmail.com>
2022-10-14 11:28:30 +0300

    add redux toolkit, test store

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,5 @@
+import { CounterSchema } from './model/types/CounterSchema';
+import { counterReducer } from './model/slices/CounterSlice';
+import { Counter } from './ui/Counter';
+
+export { CounterSchema, counterReducer, Counter };
